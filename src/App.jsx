b507943dc16bf50e964c42379d64db16f4ae541b
Blockchain Enthusiast <zakriya.mmd5@gmail.com>
2024-01-12 23:39:33 +0500

    Done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import "./App.css";
+import { ToDo_List} from "./Components/Todo";
+
+const App = () => {
+ 
+  return (
+    <>
+      
+      <ToDo_List />;
+    </>
+  );
+};
+
+export default App;
